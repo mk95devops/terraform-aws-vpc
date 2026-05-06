7413c8b02c9f1f1666e64ce4fc8bc4e94706931d
mk95devops <khamidovmaruf@gmail.com>
2026-05-06 16:25:08 -0500

    added more subnets

--- main.tf
+++ main.tf
@@ -6,11 +6,28 @@ resource "aws_vpc" "main" {
     Name = "main"
   }
 }
-resource "aws_subnet" "main" {
+resource "aws_subnet" "private1" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.1.0/24"
 
   tags = {
-    Name = "Main"
+    Name = "prvate1"
+  }
+}
+
+resource "aws_subnet" "private2" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Name = "prvate2"
+  }
+}
+resource "aws_subnet" "private3" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.3.0/24"
+
+  tags = {
+    Name = "prvate3"
   }
 }
